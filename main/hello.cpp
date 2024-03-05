1430d5618b545f3d69a386d5e78cec8fcf870a84
nik2203 <admin2@Nikhil>
2024-03-05 22:24:25 +0530

    test

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "This is a test message\n";
+  cout << "This is a testing message\n";
   return 0;
 }
